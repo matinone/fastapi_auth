cab976cadb2b808f8c8fe0c8b1f2df65f0560c64
mbrignone <mnbrignone@gmail.com>
2022-10-13 11:29:45 -0300

    Add CORS middleware to allow requests from any origin

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database.db import engine, Base
 from app.api.api import api_router
@@ -8,5 +9,14 @@ Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(title="Simple To-Do API")
+
+# Add CORS middleware to allow requests from any origin (public API)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_credentials=True,   # not compatible with allow_origins=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api")
