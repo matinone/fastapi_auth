5f4c3b696347d039e1fa6a486fe20382ced5f432
mbrignone <mnbrignone@gmail.com>
2023-03-11 16:28:36 -0300

    Fix Google login/register endpoint
    They only need to validate the Google token sent by the web app,
    the redirection is handled in the Frontend.

--- app/api/endpoints/login_google.py
+++ app/api/endpoints/login_google.py
@@ -1,88 +1,48 @@
 import secrets
 import string
 
-from authlib.integrations.starlette_client import OAuth, OAuthError
-from fastapi import Depends, FastAPI, HTTPException, Request, status
+import requests
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from starlette.config import Config
-from starlette.middleware.sessions import SessionMiddleware
 
 from app import models, schemas
 from app.api import dependencies
-from app.core.config import get_settings
 
 from .login import _check_active_user_exists, _generate_token_response
 
-google_auth_app = FastAPI()
-
-# add Session middleware so Authlib can access the request session
-google_auth_app.add_middleware(SessionMiddleware, secret_key=get_settings().SECRET_KEY)
+router = APIRouter(prefix="", tags=["login"])
 
 
-def setup_google_oauth(settings):
-    config_data = {
-        "GOOGLE_CLIENT_ID": settings.GOOGLE_CLIENT_ID,
-        "GOOGLE_CLIENT_SECRET": settings.GOOGLE_CLIENT_SECRET,
-    }
-    starlette_config = Config(environ=config_data)
-
-    oauth = OAuth(starlette_config)
-    oauth.register(
-        name="google",
-        server_metadata_url=(
-            "https://accounts.google.com/.well-known/openid-configuration"
-        ),
-        client_kwargs={"scope": "openid email profile"},
+def _validate_google_token(token: str) -> dict[str, str]:
+    headers = {"Authorization": f"Bearer {token}"}
+    resp = requests.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo", headers=headers
     )
 
-    return oauth
-
-
-oauth = setup_google_oauth(get_settings())
-
-
-async def _validate_google_token(request: Request) -> dict[str, str]:
-    try:
-        token = await oauth.google.authorize_access_token(request)
-    except OAuthError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    user_data = token.get("userinfo")
-    if not user_data:
+    if resp.status_code != 200:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Incorrect email or password",
         )
 
-    return user_data
+    return resp.json()
 
 
-@google_auth_app.get("/login_google")
-async def login_google(request: Request):
-    redirect_uri = request.url_for("get_access_token_from_google")
-    return await oauth.google.authorize_redirect(request, redirect_uri)
-
-
-# this endpoint must match an Authorized redirect URI in Google Cloud
-@google_auth_app.get(
+@router.post(
     "/token_google",
     response_model=schemas.Token,
     summary="Validate Google token and get a new API access token",
     response_description="The access token",
 )
-async def get_access_token_from_google(
-    request: Request,
+def get_access_token_from_google(
+    access_token: str = Body(embed=True),
     db: Session = Depends(dependencies.get_db),
 ):
     """
     Get an OAuth2 access token from a user logging with Google,
     to use in future requests as an authenticated user.
     """
-    user_data = await _validate_google_token(request)
+    user_data = _validate_google_token(access_token)
     user = models.User.get_by_email(db, email=user_data.get("email"))
     _check_active_user_exists(user)
 
@@ -90,25 +50,18 @@ async def get_access_token_from_google(
     return response
 
 
-@google_auth_app.get("/register_google")
-async def register_google(request: Request):
-    redirect_uri = request.url_for("create_user_from_google")
-    return await oauth.google.authorize_redirect(request, redirect_uri)
-
-
-# this endpoint must match an Authorized redirect URI in Google Cloud
-@google_auth_app.post(
-    "/new_user_google",
+@router.post(
+    "/register_google",
     response_model=schemas.User,
     status_code=status.HTTP_201_CREATED,
     summary="Validate Google token and create a new user",
-    response_description="The new created user",
+    response_description="The created user",
 )
-async def create_user_from_google(
-    request: Request,
+def create_user_from_google(
+    access_token: str = Body(embed=True),
     db: Session = Depends(dependencies.get_db),
 ):
-    user_data = await _validate_google_token(request)
+    user_data = _validate_google_token(access_token)
     user = models.User.get_by_email(db, email=user_data.get("email"))
     if user:
         raise HTTPException(
@@ -125,4 +78,5 @@ async def create_user_from_google(
 
     # user already verified if registering using Google
     user = models.User.create(db, user_create, is_verified=True)
+
     return user
--- app/main.py
+++ app/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.api import api_router
-from app.api.endpoints.login_google import google_auth_app
 from app.core.security import get_settings
 from app.database.init_db import init_db
 
 if get_settings().ENVIRONMENT != "test":
-    # create DB tables (would be better to use Alembic) and superuser
     init_db()
 
 app = FastAPI(title="Simple To-Do API")
@@ -22,7 +20,3 @@ app.add_middleware(
 )
 
 app.include_router(api_router, prefix="/api")
-
-# use a subapp so the Session middleware required for Google authentication
-# is only present in those endpoints
-app.mount("/api", google_auth_app)
